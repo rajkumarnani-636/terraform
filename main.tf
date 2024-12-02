41fd94cb4f4beab47078a41ad6190248aceb18c2
rajkumarnani-636 <rajkumarnani636@gmail.com>
2024-12-02 12:14:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "one" {
-  ami           = "ami-02a0945ba27a488b7"
+  ami           = "ami-0166fe664262f664c"
   instance_type = "t2.micro"
   tags = {
     Name = "raham"
